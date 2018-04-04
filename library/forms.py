cd8103261fc6e1fc0755295076b1acf9f9c70d7e
Natasha <nata.tupikina.nt@gmail.ru>
2018-04-04 23:56:41 +0300

    я пыталась убрать, что надобавлялось

--- library/forms.py
+++ library/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Login, User
+from .models import Login, User, Book
 
 
 class LoginForm(forms.ModelForm):
@@ -17,15 +17,15 @@ class SignUpForm(forms.ModelForm):
         exclude = [""]
 
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('login', 'password', 'first_name', 'second_name', 'address', 'phone_number')
-        exclude = [""]
+#class UserForm(forms.ModelForm):
+#    class Meta:
+ #       model = User
+  #      fields = ('login', 'password', 'first_name', 'second_name', 'address', 'phone_number')
+   #     exclude = [""]
 
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ('library','title', 'price_value', 'is_best_seller', 'edition', 'publisher', 'publish_time')
-        exclude = [""]
+#class BookForm(forms.ModelForm):
+ #   class Meta:
+  #      model = Book
+   #     fields = ('library','title', 'price_value', 'is_best_seller', 'edition', 'publisher', 'publish_time')
+    #    exclude = [""]
--- library/urls.py
+++ library/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'libsystem_logined/', views.logined_library, name='libsystem_logined'),
     url(r'^login', views.login, name='login_account'),
     url(r'^signup', views.signup, name='signup_account'),
-    url(r'^user_list', views.list_of_books, name='user_list'),
-    url(r'^librarian_list', views.list_to_delete, name='librarian_list'),
-    url(r'^librarian_add', views.librarian_add, name='librarian_add'),
+#    url(r'^user_list', views.list_of_books, name='user_list'),
+ #   url(r'^librarian_list', views.list_to_delete, name='librarian_list'),
+  #  url(r'^librarian_add', views.librarian_add, name='librarian_add'),
 ]
