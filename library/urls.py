89600dbbdd25e18a90438235c36aa543d0cdd8cb
Natasha <nata.tupikina.nt@gmail.ru>
2018-04-22 16:52:45 +0300

    we need dis

--- library/urls.py
+++ library/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.library, name='libsystem'),
     url(r'libsystem_logined/', views.logined_library, name='libsystem_logined'),
-    path('accounts/', include('django.contrib.auth.urls')),
     url(r'^login', views.login, name='login'),
     url(r'^signup', views.signup, name='signup'),
     url(r'^login_not_valid', views.login_not_valid, name='login_not_valid'),
@@ -16,4 +15,5 @@ urlpatterns = [
     url(r'^books_for_user', views.books_for_user, name='books_for_user'),
     path('users/<int:pk>/', views.user_detail.as_view(), name='user_detail'),
     path('users/<int:pk>/delete_user/', views.librarian_delete_user, name='delete_user'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
